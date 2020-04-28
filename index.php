b131b3b857f34ea4a3b68072363a9d1e4ab53cdc
Isabelle Titaw <46397217+IsaTitaw@users.noreply.github.com>
2020-04-28 12:16:27 +0200

    update
    Remove HTML

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 spl_autoload_register(function($class) {
-    var_dump('in autoload', $class);
+//    var_dump('in autoload', $class);
     if($class == "Router") {
         include "router/Router.php";
     } else if (strpos($class, "Controller")) {
@@ -12,17 +12,8 @@ spl_autoload_register(function($class) {
     }
 });
 
-
 $router = new Router($_GET, $_POST, $_SERVER['PHP_SELF'], $_SERVER['REQUEST_URI']);
-var_dump($_GET);
-var_dump($_POST);
-var_dump($_SERVER['PHP_SELF']);  //chemin du fichier sur lequel je suis ici index.php dû au htaccess
-var_dump($_SERVER['REQUEST_URI']); //morceau de l'url encodé pour accéder au site
-
-//var_dump($router);
 ?>
 
-<h1>Index</h1
-
-//grâce à la configuration dans htaccess, tout est redirigé vers index.php même localhost/routeur2/jkqzbcqzgfezhjuysqe
-    //du coup ça lance new Router et "new" déclenche le fonction spl_autoload_register qui va donc inclure le fichier correspondant par exemple router.php
+<!--//grâce à la configuration dans htaccess, tout est redirigé vers index.php même localhost/routeur2/jkqzbcqzgfezhjuysqe-->
+<!--    //du coup ça lance new Router et "new" déclenche le fonction spl_autoload_register qui va donc inclure le fichier correspondant par exemple router.php-->
